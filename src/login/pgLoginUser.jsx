36ab30aea1dd039c9402068f5dbda5e62d6311d2
Liviacavalcanter <liviacavalcantebr@gmail.com>
2025-06-02 11:48:03 -0300

    mudança de implementação na tela registro

--- src/login/pgLoginUser.jsx
+++ src/login/pgLoginUser.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"; // Importando o hook de navegação
+import { useNavigate } from "react-router-dom"; 
+import { Eye, EyeOff } from "lucide-react";
 import FooterLinks from "./components/footerLinks"
 
 export default function LoginForm() {
   const [loginError, setLoginError] = useState("");
   const [email, setEmail] = useState("");
   const [senha, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(true);
   const [error, setError] = useState("");
   const navigate = useNavigate(); // Criando a função de navegação
   const handleLogin = async (event) => {
@@ -50,10 +52,9 @@ export default function LoginForm() {
       }
     } catch (error) {
       console.error("Erro na requisição:", error);
-      setLoginError(data.mensagem || "Email ou senha incorretos!");
+      setLoginError("Erro na requisição. Verifique sua conexão ou tente novamente.");
     }
   };
-
 
   return (
     <main className="flex justify-center items-center h-screen bg-cover bg-center text-white"
@@ -78,15 +79,24 @@ export default function LoginForm() {
             />
           </div>
           <div className="mb-3">
-            <label className="block text-left mb-1">Senha</label>
+          <label className="block text-left mb-1">Senha</label>
+          <div className="relative">
             <input
-              type="password"
+              type={showPassword ? "password" : "text"}
               value={senha}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-2 border border-gray-400 rounded bg-gray-200 text-gray-900"
+              className="w-full p-2 pr-10 border border-gray-400 rounded bg-gray-200 text-gray-900"
               placeholder="Digite sua senha"
               required
             />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute inset-y-0 right-2 flex items-center"
+            >
+              {showPassword ? <EyeOff className="h-5 w-5 text-gray-600" /> : <Eye className="h-5 w-5 text-gray-600" />}
+            </button>
+          </div>
           </div>
           <div className="flex items-center mb-3">
             <input type="checkbox" id="remember" className="mr-2" />
--- src/registro/component/register-form.jsx
+++ src/registro/component/register-form.jsx
@@ -1,57 +1,27 @@
-import { useState } from "react"
-import { Eye, EyeOff } from "lucide-react"
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-
 import FooterLinks from "./footerLinks";
-
 
 function RegisterForm() {
   const [backendErrors, setBackendErrors] = useState([]);
   const [formData, setFormData] = useState({
-    nome_completo: "",
-    nome_usuario: "",
-    data_nascimento: "",
+    nome_fantasia: "",
+    telefone: "",
     email: "",
     senha: "",
     confirmPassword: "",
-    //tipo: "usuario"
-  })
+  });
 
-  const [showPassword, setShowPassword] = useState(false)
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
-  const [errorDataNascimento, setErrorDataNascimento] = useState("");
+  const [showPassword, setShowPassword] = useState(true);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(true);
   const [errorEmail, setErrorEmail] = useState("");
   const navigate = useNavigate();
-  //const tipos = [
-  //{ label: 'Usuário', value: 'usuario' },
-  //{ label: 'Estabelecimento', value: 'servico' }
-  //];
 
   const handleChange = (e) => {
-    const { name, value } = e.target
-
-    if (name === "data_nascimento") {
-      let onlyNumbers = value.replace(/\D/g, "");
-      if (onlyNumbers.length > 8) {
-        onlyNumbers = onlyNumbers.slice(0, 8);
-      }
-      let masked = onlyNumbers;
-      if (onlyNumbers.length >= 5) {
-        masked = `${onlyNumbers.slice(0, 2)}/${onlyNumbers.slice(2, 4)}/${onlyNumbers.slice(4)}`;
-      } else if (onlyNumbers.length >= 3) {
-        masked = `${onlyNumbers.slice(0, 2)}/${onlyNumbers.slice(2)}`;
-      }
-      setFormData((prev) => ({
-        ...prev,
-        [name]: masked,
-      }));
-      // Verifique a validade da data após a mudança
-      if (isDateValid(masked)) {
-        setErrorDataNascimento(""); // Se válido, limpe o erro
-      } else {
-        setErrorDataNascimento("Data inválida. OBS: Mínimo 6 anos.");
-      }
-    } else if (name === "email") {
+    const { name, value } = e.target;
+  
+    if (name === "email") {
       setFormData((prev) => ({
         ...prev,
         [name]: value,
@@ -61,55 +31,49 @@ function RegisterForm() {
       } else {
         setErrorEmail("Email inválido.");
       }
-      //} else if (name === "tipo") {
-      //setFormData((prev) => ({
-      //...prev,
-      //tipo: value
-      //}));
-      //return;
+    } else if (name === "telefone") {
+      const telefoneFormatado = formatarTelefone(value);
+      setFormData((prev) => ({
+        ...prev,
+        telefone: telefoneFormatado,
+      }));
     } else {
       setFormData((prev) => ({
         ...prev,
         [name]: value,
       }));
     }
-  };
-
-  const isDateValid = (data) => {
-    const [dia, mes, ano] = data.split("/").map(Number);
-    const nascimento = new Date(ano, mes - 1, dia);
-    const hoje = new Date();
-    const diffAnos = hoje.getFullYear() - nascimento.getFullYear();
-    const isValid = diffAnos > 6 ||
-      (diffAnos === 6 &&
-        (hoje.getMonth() > nascimento.getMonth() ||
-          (hoje.getMonth() === nascimento.getMonth() && hoje.getDate() >= nascimento.getDate())));
-    return (
-      data.length === 10 &&
-      !isNaN(nascimento.getTime()) &&
-      isValid
-    );
-  };
+  };  
 
   const isEmailValid = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
   };
 
+  const formatarTelefone = (valor) => {
+    const numero = valor.replace(/\D/g, "");
+  
+    if (numero.length <= 10) {
+      return numero.replace(/(\d{2})(\d{4})(\d{0,4})/, "($1) $2-$3");
+    } else {
+      return numero.replace(/(\d{2})(\d{5})(\d{0,4})/, "($1) $2-$3");
+    }
+  };  
+
   const handleSubmit = async (e) => {
     setBackendErrors([]);
-    e.preventDefault()
+    e.preventDefault();
 
-    if (!formData.nome_completo || !formData.nome_usuario || !formData.data_nascimento || !formData.email || !formData.senha || !formData.confirmPassword /* || !formData.tipo */) {
-      alert("Preencha todos os campos.")
-      return
-    }
-
-    if (!isDateValid(formData.data_nascimento)) {
-      setErrorDataNascimento("A data deve ser válida e do passado, com ao menos 6 anos.");
+    if (
+      !formData.nome_fantasia ||
+      !formData.telefone ||
+      !formData.email ||
+      !formData.senha ||
+      !formData.confirmPassword
+    ) {
+      alert("Preencha todos os campos.");
       return;
     }
-    setErrorDataNascimento("");
 
     if (!isEmailValid(formData.email)) {
       setErrorEmail("Email inválido.");
@@ -118,29 +82,27 @@ function RegisterForm() {
     setErrorEmail("");
 
     if (formData.senha !== formData.confirmPassword) {
-      alert("As senhas não coincidem.")
-      return
+      alert("As senhas não coincidem.");
+      return;
     }
 
     console.log("Dados validados:", formData);
     try {
-      const response = await fetch("http://localhost:3000/rest/v1/usuarios", {
+      const response = await fetch("http://localhost:3000/rest/v1/usuariosOrg", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          nome_completo: formData.nome_completo,
-          nome_usuario: formData.nome_usuario,
-          data_nascimento: formData.data_nascimento,
+          nome_fantasia: formData.nome_fantasia,
+          telefone: formData.telefone,
           email: formData.email,
           senha: formData.senha,
-          //tipo: formData.tipo,
         }),
-      })
-      const dados = await response.json()
-      console.log("Status:", dados.status)
-      console.log("Resposta do servidor:", dados)
+      });
+
+      const dados = await response.json();
+      console.log("Resposta do servidor:", dados);
       if (!response.ok) {
         if (dados.errors) {
           setBackendErrors(dados.errors);
@@ -150,12 +112,12 @@ function RegisterForm() {
         return;
       }
 
-      alert("Usuário registrado com sucesso!")
+      alert("Usuário registrado com sucesso!");
       navigate("/loginUser");
     } catch (err) {
-      alert(err.message)
+      alert(err.message);
     }
-  }
+  };
 
   const inputStyle = "w-full px-3 pt-4 p-2 pb-1 text-base border border-white rounded bg-[#333] text-white peer focus:outline-none focus:ring-2 focus:ring-[#00dac1] focus:border-transparent";
   const inputLabelStyle = `absolute left-3 text-base text-gray-400 duration-150 transform origin-[0] pointer-events-none transition-all
@@ -165,13 +127,11 @@ function RegisterForm() {
     peer-focus:top-0 
     peer-focus:translate-y-0 
     peer-focus:scale-75
-    ${formData.nome_usuario !== '' ? 'top-0 translate-y-0 scale-75' : ''}
+    ${formData.nome_fantasia !== '' ? 'top-0 translate-y-0 scale-75' : ''}
   `;
 
-
-
   return (
-    <div className="w-full max-w-[330px] h-auto max-h-[97] overflow-auto text-center p-6 bg-[rgba(0,0,0,0.7)] rounded-[10px] shadow-[0_0_10px_rgba(233,233,233,0.621)] text-white">
+    <div className="w-full max-w-[330px] h-auto text-center p-6 bg-[rgba(0,0,0,0.7)] rounded-[10px] shadow-[0_0_10px_rgba(233,233,233,0.621)] text-white">
       <div className="relative -mt-8 mb-1 w-28 h-20 mx-auto">
         <img src="/imagens/icone_travo.png" alt="Logo TRAVO" width={110} height={110} className="mx-auto" />
       </div>
@@ -182,69 +142,45 @@ function RegisterForm() {
 
       <form onSubmit={handleSubmit} className="w-full">
         <div className="mb-3 relative">
-          <label className="block text-left text-[15px] mb-0.5" htmlFor="nome_completo">Nome</label>
+          <label className="block text-left text-[15px] mb-0.5" htmlFor="nome_fantasia">Nome</label>
           <div className="relative">
             <input
               type="text"
-              id="nome_completo"
-              name="nome_completo"
-              value={formData.nome_completo}
+              id="nome_fantasia"
+              name="nome_fantasia"
+              value={formData.nome_fantasia}
+              onChange={handleChange}
+              className={inputStyle}
+              placeholder=" "
+              required
+            />
+            <label 
+            htmlFor="nome_fantasia" 
+            className={inputLabelStyle}
+            >
+              Digite o nome da organização
+            </label>
+          </div>
+        </div>
+
+        <div className="mb-3 relative">
+          <label className="block text-left text-[15px] mb-0.5" htmlFor="telefone">Telefone</label>
+          <div className="relative">
+            <input
+              type="tel"
+              id="telefone"
+              name="telefone"
+              value={formData.telefone}
               onChange={handleChange}
               className={inputStyle}
               placeholder=" "
               required
             />
             <label
-              htmlFor="nome_completo"
-              className={inputLabelStyle}
+              htmlFor="telefone"
+              className={`${inputLabelStyle} ${formData.telefone !== '' ? 'top-0 translate-y-0 scale-75' : ''}`}
             >
-              Digite seu nome completo
-            </label>
-          </div>
-        </div>
-
-        <div className="mb-3 relative">
-          <label className="block text-left text-[15px] mb-0.5" htmlFor="nome_usuario">Nome de usuário</label>
-          <div className="relative">
-            <input
-              type="text"
-              id="nome_usuario"
-              name="nome_usuario"
-              value={formData.nome_usuario}
-              onChange={handleChange}
-              className={inputStyle}
-              placeholder=" "
-              required
-            />
-            <label
-              htmlFor="nome_usuario"
-              className={inputLabelStyle}
-            >
-              Digite seu nome público
-            </label>
-          </div>
-        </div>
-
-        <div className="mb-3 relative">
-          <label className="block text-left text-[15px] mb-0.5" htmlFor="data_nascimento">Data de nascimento</label>
-          <div className="relative">
-            <input
-              type="text"
-              id="data_nascimento"
-              name="data_nascimento"
-              value={formData.data_nascimento}
-              onChange={handleChange}
-              className={`${inputStyle} ${errorDataNascimento ? 'border-red-500' : ''}`}
-              maxLength={10}
-              placeholder=" "
-              required
-
-            />
-            <label
-              htmlFor="data_nascimento"
-              className={`${inputLabelStyle} ${formData.data_nascimento !== '' ? 'top-0 translate-y-0 scale-75' : ''} ${errorDataNascimento ? 'text-red-400' : ''}`}
-            >
-              {errorDataNascimento || 'Digite sua data de nascimento'}
+              Digite seu telefone
             </label>
           </div>
         </div>
@@ -275,7 +211,7 @@ function RegisterForm() {
           <label className="block text-left text-[15px] mb-0.5" htmlFor="senha">Senha</label>
           <div className="relative">
             <input
-              type={showPassword ? "text" : "senha"}
+              type={showPassword ? "password" : "text"}
               id="senha"
               name="senha"
               value={formData.senha}
@@ -299,9 +235,9 @@ function RegisterForm() {
             </button>
           </div>
 
-          <div className="mt-1 relative" htmlFor="confirmPassword">
+          <div className="mt-1 relative">
             <input
-              type={showConfirmPassword ? "text" : "confirmPassword"}
+              type={showConfirmPassword ? "password" : "text"}
               id="confirmPassword"
               name="confirmPassword"
               value={formData.confirmPassword}
@@ -310,9 +246,9 @@ function RegisterForm() {
               placeholder=""
               required
             />
-            <label
-              htmlFor="confirmPassword"
-              className={inputLabelStyle}
+            <label 
+            htmlFor="confirmPassword" 
+            className={inputLabelStyle}
             >
               Confirme sua senha
             </label>
@@ -321,7 +257,7 @@ function RegisterForm() {
               onClick={() => setShowConfirmPassword(!showConfirmPassword)}
               className="absolute inset-y-0 right-0 flex items-center pr-3"
             >
-              {showConfirmPassword ? (
+    {showConfirmPassword ? (
                 <EyeOff className="h-5 w-5 text-gray-400" />
               ) : (
                 <Eye className="h-5 w-5 text-gray-400" />
@@ -347,8 +283,8 @@ function RegisterForm() {
         </button>
       </form>
     </div>
-  )
+  );
 }
 
 
-export default RegisterForm
+export default RegisterForm;
